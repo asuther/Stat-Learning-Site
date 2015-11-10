c8084e43333460cc15cab26ed8015aa75646179a
asuther <alexander.m.sutherland@gmail.com>
2015-11-10 01:08:38 -0800

    Added text to linear regression lesson and display standard error

--- js/controller-lesson-2.js
+++ js/controller-lesson-2.js
@@ -2,6 +2,8 @@ var g = new graph();
 
 var pointsX = [20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300];
 var pointsY = [5.323169, 32.116875, 37.512964, 18.827295, 35.767723, 8.126310, 88.338183, 56.471593, 82.898370, 110.501148, 133.672267, 129.001325, 137.316908, 153.702134, 142.017148];
+var currentRSS;
+var beta;
 
 g.drawPoints(pointsX, pointsY);
 g.drawLine([20,100],[30,50], true);
@@ -10,9 +12,13 @@ g.drawReferencePoints([20,100],[30,50]);
 //Custom Functions for lesson
 function calculateBeta() {
   referencePoints = g.getReferencePoints();
-    slope = ( referencePoints[1][1] - referencePoints[0][1] ) / ( referencePoints[1][0] - referencePoints[0][0] );
-    $('#betaValue').text(slope.toFixed(2));
+    beta = ( referencePoints[1][1] - referencePoints[0][1] ) / ( referencePoints[1][0] - referencePoints[0][0] );
+    $('#betaValue').text(beta.toFixed(2));
 };
+function calculateSEBeta() {
+    $('#SEValue').text(Math.sqrt(currentRSS / (13 * beta)).toFixed(1));
+};
+
 var updateRSS = function() {
     currentRSS = g.getRSS();
     $('#totalRSS').text(Math.round(currentRSS));
@@ -39,6 +45,7 @@ graphCanvas.mousedown(function(e) {
         g.mousemove(e);
         calculateBeta();
         updateRSS();
+        calculateSEBeta();
     } else {
         g.mousemoveNoClick(e);
     }
