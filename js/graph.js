3d677d0479873ca10f181ff44b17057d238cae67
asuther <alexander.m.sutherland@gmail.com>
2015-11-10 21:08:10 -0800

    Graph and LinearRegressionModel Objects split up
    The Graph object has had all functions related to linear regression
    removed. Now, its sole function is to be a graph.
    
    The linear regression functions were moved to a LinearRegressionModel
    object. This object can be initialized and added to the Graph object as
    a "dependency.' The graph object will act as a subject with dependencies
    as observers.

--- js/graph.js
+++ js/graph.js
@@ -1,5 +1,10 @@
-function graph() {
-
+function graph(canvasID) {
+    var graphParams = {
+        leftOffset: 50,
+        graphHeight: 300,
+        graphWidth: 300,
+        pointsSizeDefault: 4
+    };
     var leftOffset = 50;
     var graphHeight = 300;
     var graphWidth = 300;
@@ -7,42 +12,37 @@ function graph() {
     var pointsX = [];
     var pointsY = [];
 
-    var referencePointsX = [];
-    var referencePointsY = [];
-
-    var canvas = $('#graph');
+    var canvas = $('#'+canvasID);
     var context = canvas.get(0).getContext('2d');
-
-    var lineStats = {
-        m: 0,
-        b: 0
-    };
-    var RSS = 99999;
 
     var isMouseDown = false;
     var that = this;
-    var closestReferencePoint = 0;
-    var closestReferenceInfo = {};
+
+
+    var dependencies = [];
 
 
     this.init = function() {
         this.drawAxes();
     };
 
-    this.update= function() {
+    this.update = function() {
 
         context.fillStyle = 'white';
         context.fillRect(0,0,500,300);
         this.drawAxes();
         this.drawPoints(this.pointsX, this.pointsY);
-        this.drawLine(referencePointsX,referencePointsY, true);
-        this.drawReferencePoints(referencePointsX,referencePointsY);
 
-        RSS = this.calculateRSS();
-        if($('#showRSS').is(':checked')) {
-            this.drawRSS();
+        for(var depIndex = 0; depIndex < dependencies.length; depIndex++) {
+            dependencies[depIndex].update(context);
         }
 
+
+    };
+
+    this.addDependency = function(dependencyObject) {
+        dependencyObject.setGraphParams(graphParams);
+        dependencies.push(dependencyObject);
     };
 
     this.drawAxes = function() {
@@ -61,7 +61,19 @@ function graph() {
         context.fillText('Value 2',-200,38);
         context.restore();
     };
+    this.setPoints = function(pointsX, pointsY) {
+        if(pointsX.length != pointsY.length) {
+            return;
+        }
+        //Store points
+        this.pointsX = pointsX;
+        this.pointsY = pointsY;
 
+        //Broadcast the points to all dependencies
+        dependencies.forEach(function(dependency) {
+            dependency.setPoints(pointsX, pointsY);
+        });
+    };
     this.drawPoints = function(pointsX, pointsY) {
         pointSize = pointSizeDefault;
         //If the arrays are not the same length, exit
@@ -72,165 +84,48 @@ function graph() {
         for (var index = 0; index < pointsX.length ; index++ ) {
             context.fillRect(pointsX[index] + leftOffset, graphHeight - pointsY[index], pointSize, pointSize);
         }
-        //Store points
-        this.pointsX = pointsX;
-        this.pointsY = pointsY;
+
     };
 
-    this.drawReferencePoints = function(pointsX, pointsY) {
-        if(pointsX.length != 2 || pointsX.length != pointsY.length) {
-            return;
-        }
-        referencePointsX = pointsX;
-        referencePointsY = pointsY;
-        //Draw first point
-
-        for (var referenceIndex = 0; referenceIndex < pointsX.length ; referenceIndex++ ) {
-            context.beginPath();
-            context.fillStyle = 'blue';
-            pointSize = pointSizeDefault;
-            if(referenceIndex == closestReferencePoint && isMouseDown){
-                context.fillStyle = 'red';
-            } else if(closestReferenceInfo.closestRefIndex == referenceIndex && closestReferenceInfo.closestRefDistance < 15) {
-                context.fillStyle = 'orange';
-                pointSize = 6;
-            }
-
-
-            context.arc(pointsX[referenceIndex] + leftOffset, graphHeight - pointsY[referenceIndex], pointSize, 0, 2 * Math.PI, false);
-            context.fill();
-            context.closePath();
-        }
-
-       // context.fillOval(pointsX[1] + leftOffset - pointSize, graphHeight - pointsY[1] - pointSize, pointSize*2, pointSize*2);
-    };
-
-    this.drawLine = function(pointsX,pointsY, extrapolate) {
-
-        extrapolate = extrapolate || false;
-        var pointsArrayLength = pointsX.length;
-        //the arrays must match in size and there must be at least two points
-        if(pointsX.length != pointsY.length || pointsArrayLength < 2) {
-            return;
-        }
-        context.strokeStyle = 'black';
-        context.beginPath();
-        if(extrapolate) {
-            //Determine y = mx + b
-            lineStats.m = ( pointsY[ pointsArrayLength - 1 ] - pointsY[ pointsArrayLength - 2 ] ) / ( pointsX[ pointsArrayLength - 1] - pointsX[ pointsArrayLength - 2 ]);
-            lineStats.b = pointsY[0] - lineStats.m * pointsX[0];
-            context.moveTo(0 + leftOffset,graphHeight - lineStats.b);
-        } else {
-            context.moveTo(pointsX[0]+ leftOffset,graphHeight - pointsY[0]);
-        }
-
-        for (var index = 1; index < pointsX.length ; index++ ) {
-            context.lineTo(pointsX[index]+ leftOffset, graphHeight - pointsY[index]);
-        }
-
-        if(extrapolate) {
-            finalY = lineStats.m  * ( graphWidth ) + lineStats.b;
-            context.lineTo(graphWidth + leftOffset, graphHeight - finalY);
-        }
-        context.stroke();
-    };
-
-    this.drawRSS = function() {
-        for (var index = 0 ; index < this.pointsX.length ; index++ ) {
-            predictedY = lineStats.m * this.pointsX[index] + lineStats.b;
-            squaredResidual = Math.pow(predictedY - this.pointsY[index], 2);
-
-            context.beginPath();
-            context.moveTo(this.pointsX[index] + leftOffset + 2, graphHeight - this.pointsY[index]);
-            context.lineTo(this.pointsX[index] + leftOffset + 2, graphHeight - predictedY);
-            context.strokeStyle = '#FF0000';
-            context.stroke();
-            context.fillStyle = 'black';
-            context.font = '9pt Calibri';
-            context.fillText(Math.round(squaredResidual), this.pointsX[index] + leftOffset + 5, graphHeight - (this.pointsY[index] + (predictedY - this.pointsY[index])/2) + 5)
-        }
-    };
-
-    this.calculateRSS = function() {
-        var RSS = 0;
-        context.strokeStyle = '#FF0000';
-        for (var index = 0 ; index < this.pointsX.length ; index++ ) {
-            predictedY = lineStats.m * this.pointsX[index] + lineStats.b;
-            squaredResidual = Math.pow(predictedY - this.pointsY[index], 2);
-            //console.log('Predicted Y: ' + predictedY);
-            //console.log('Actual Y: ' + this.pointsY[index]);
-            //console.log('Sq Res: ' + squaredResidual);
-            RSS += squaredResidual;
-         }
-        return RSS;
-    };
-
-    this.getRSS = function() {
-        return this.calculateRSS();
-    };
-    this.getClosestReferencePoint = function(pageClickedX, pageClickedY, referencePointsX, referencePointsY) {
-        var distances = [0,0];
-        var shortestDistance = 2000000;
-        var shortestDistanceIndex = -1;
-        //console.log('(' + pageClickedX + ", " + pageClickedY + ')');
-        clickedX = (pageClickedX - leftOffset);
-        clickedY = (graphHeight - pageClickedY);
-        //console.log('(' + clickedX + ", " + clickedY + ')');
-        for (var index = 0; index < referencePointsX.length ; index++ ) {
-
-            // context.beginPath();
-
-            // context.moveTo(clickedX + leftOffset, graphHeight - clickedY);
-            // context.lineTo(referencePointsX[index] + leftOffset, graphHeight - referencePointsY[index]);
-            // context.strokeStyle = '#FF0000';
-            // context.stroke();
-
-            currentDistance = Math.sqrt(  Math.pow(clickedX - referencePointsX[index] , 2) + Math.pow(clickedY - referencePointsY[index], 2) );
-            if(currentDistance < shortestDistance) {
-                shortestDistance = currentDistance;
-                shortestDistanceIndex = index;
-            }
-        }
-        return {'closestRefIndex':shortestDistanceIndex, 'closestRefDistance': shortestDistance};
-    };
 
     this.mousedown = function(e) {
         //console.log('keydown');
         isMouseDown = true;
         //calculated distance from
-
-        closestReferencePoint = that.getClosestReferencePoint(e.pageX, e.pageY, referencePointsX, referencePointsY).closestRefIndex;
+        dependencies.forEach(function(dependency) {
+            dependency.mousedown(e);
+            dependency.update(context);
+        });
         //console.log('Closest Ref Index: ' + closestReferencePoint);
     };
 
     this.mouseup = function(e) {
-        //console.log('keyup');
-        $('body').css('cursor','default');
-        closestReferencePoint = -1;
         isMouseDown = false;
         that.update();
+
+        dependencies.forEach(function(dependency) {
+            dependency.mouseup(e);
+            dependency.update(context);
+        });
     };
 
     this.mousemove = function(e) {
-        if(isMouseDown){
-            $('body').css('cursor','none');
-            //  console.log(e.pageX + ", " + e.pageY);
-            referencePointsX[closestReferencePoint] = e.pageX - leftOffset;
-            referencePointsY[closestReferencePoint] = graphHeight - e.pageY;
-            that.update();
-        }
-
+        that.update();
+        dependencies.forEach(function(dependency) {
+            dependency.mousemove(e);
+            dependency.update(context);
+        });
     };
     this.mousemoveNoClick = function(e) {
-        closestReferenceInfo = that.getClosestReferencePoint(e.pageX, e.pageY, referencePointsX, referencePointsY);
+
         that.update();
+        dependencies.forEach(function(dependency) {
+            dependency.mousemoveNoClick(e);
+            dependency.update(context);
+        });
     };
     this.getCanvas = function() {
         return canvas;
-    };
-
-    this.getReferencePoints = function() {
-        return [ [referencePointsX[0], referencePointsY[0]], [referencePointsX[1], referencePointsY[1]] ];
     };
 
     this.init();
---
+++ js/linearRegressionModel.js
@@ -0,0 +1,195 @@
+function LinearRegressionModel() {
+
+    var referencePointsX = [];
+    var referencePointsY = [];
+    var graphParams = {};
+
+    var lineStats = {
+        m: 0,
+        b: 0
+    };
+    var RSS = 99999;
+
+    var closestReferencePoint = 0;
+    var closestReferenceInfo = {};
+
+    var isMouseDown = false;
+    var that = this;
+
+    var pointsX;
+    var pointsY;
+
+    this.init = function() {
+        this.setReferencePoints([20,100],[30,50]);
+    };
+    this.setGraphParams = function(graphParams) {
+        this.graphParams = graphParams;
+    };
+
+    this.setReferencePoints = function(referencePointsX, referencePointsY) {
+        if(referencePointsX.length != 2 || referencePointsX.length != referencePointsY.length) {
+            return;
+        }
+        this.referencePointsX = referencePointsX;
+        this.referencePointsY = referencePointsY;
+        console.log("reference X");
+        console.log(this.referencePointsX);
+        //Draw first point
+    };
+    this.update = function(context) {
+        this.drawLine(context, this.referencePointsX, this.referencePointsY, true);
+        this.drawReferencePoints(context, this.referencePointsX, this.referencePointsY);
+
+        RSS = this.calculateRSS();
+
+        if($('#showRSS').is(':checked')) {
+            this.drawRSS(context);
+        }
+    };
+
+    this.drawReferencePoints = function(context, pointsX, pointsY) {
+
+
+        for (var referenceIndex = 0; referenceIndex < pointsX.length ; referenceIndex++ ) {
+            context.beginPath();
+            context.fillStyle = 'blue';
+            pointSize = pointSizeDefault;
+            if(referenceIndex == closestReferencePoint && isMouseDown){
+                context.fillStyle = 'red';
+            } else if(closestReferenceInfo.closestRefIndex == referenceIndex && closestReferenceInfo.closestRefDistance < 15) {
+                context.fillStyle = 'orange';
+                pointSize = 6;
+            }
+
+
+            context.arc(pointsX[referenceIndex] + this.graphParams.leftOffset, this.graphParams.graphHeight - pointsY[referenceIndex], pointSize, 0, 2 * Math.PI, false);
+            context.fill();
+            context.closePath();
+        }
+
+       // context.fillOval(pointsX[1] + leftOffset - pointSize, graphHeight - pointsY[1] - pointSize, pointSize*2, pointSize*2);
+    };
+
+    this.drawLine = function(context, refPointsX, refPointsY, extrapolate) {
+        console.log(refPointsX);
+        var pointsArrayLength = refPointsX.length;
+        extrapolate = extrapolate || false;
+        context.strokeStyle = 'black';
+
+        context.beginPath();
+        if(extrapolate) {
+            //Determine y = mx + b
+            lineStats.m = ( refPointsY[ pointsArrayLength - 1 ] - refPointsY[ pointsArrayLength - 2 ] ) / ( refPointsX[ pointsArrayLength - 1] - refPointsX[ pointsArrayLength - 2 ]);
+            lineStats.b = refPointsY[0] - lineStats.m * refPointsX[0];
+            context.moveTo(0 + this.graphParams.leftOffset,this.graphParams.graphHeight - lineStats.b);
+        } else {
+            context.moveTo(refPointsX[0]+ this.graphParams.leftOffset,this.graphParams.graphHeight - refPointsY[0]);
+        }
+
+        for (var index = 1; index < refPointsX.length ; index++ ) {
+            context.lineTo(refPointsX[index]+ this.graphParams.leftOffset, this.graphParams.graphHeight - refPointsY[index]);
+        }
+
+        if(extrapolate) {
+            finalY = lineStats.m  * ( this.graphParams.graphWidth ) + lineStats.b;
+            context.lineTo(this.graphParams.graphWidth + this.graphParams.leftOffset, this.graphParams.graphHeight - finalY);
+        }
+        context.stroke();
+    };
+
+    this.drawRSS = function(context) {
+        for (var index = 0 ; index < this.pointsX.length ; index++ ) {
+            predictedY = lineStats.m * this.pointsX[index] + lineStats.b;
+            squaredResidual = Math.pow(predictedY - this.pointsY[index], 2);
+
+            context.beginPath();
+            context.moveTo(this.pointsX[index] + this.graphParams.leftOffset + 2, this.graphParams.graphHeight - this.pointsY[index]);
+            context.lineTo(this.pointsX[index] + this.graphParams.leftOffset + 2, this.graphParams.graphHeight - predictedY);
+            context.strokeStyle = '#FF0000';
+            context.stroke();
+            context.fillStyle = 'black';
+            context.font = '9pt Calibri';
+            context.fillText(Math.round(squaredResidual), this.pointsX[index] + this.graphParams.leftOffset + 5, this.graphParams.graphHeight - (this.pointsY[index] + (predictedY - this.pointsY[index])/2) + 5)
+        }
+    };
+
+    this.calculateRSS = function() {
+        var RSS = 0;
+        //context.strokeStyle = '#FF0000';
+        for (var index = 0 ; index < this.pointsX.length ; index++ ) {
+            predictedY = lineStats.m * this.pointsX[index] + lineStats.b;
+            squaredResidual = Math.pow(predictedY - this.pointsY[index], 2);
+            //console.log('Predicted Y: ' + predictedY);
+            //console.log('Actual Y: ' + this.pointsY[index]);
+            //console.log('Sq Res: ' + squaredResidual);
+            RSS += squaredResidual;
+         }
+        return RSS;
+    };
+
+    this.getRSS = function() {
+        return this.calculateRSS();
+    };
+    this.getClosestReferencePoint = function(pageClickedX, pageClickedY, referencePointsX, referencePointsY) {
+
+        var distances = [0,0];
+        var shortestDistance = 2000000;
+        var shortestDistanceIndex = -1;
+        //console.log('(' + pageClickedX + ", " + pageClickedY + ')');
+        clickedX = (pageClickedX - this.graphParams.leftOffset);
+        clickedY = (this.graphParams.graphHeight - pageClickedY);
+        //console.log('(' + clickedX + ", " + clickedY + ')');
+        for (var index = 0; index < referencePointsX.length ; index++ ) {
+
+            currentDistance = Math.sqrt(  Math.pow(clickedX - referencePointsX[index] , 2) + Math.pow(clickedY - referencePointsY[index], 2) );
+            if(currentDistance < shortestDistance) {
+                shortestDistance = currentDistance;
+                shortestDistanceIndex = index;
+            }
+        }
+        return {'closestRefIndex':shortestDistanceIndex, 'closestRefDistance': shortestDistance};
+    };
+
+    this.mousedown = function(e) {
+        console.log('keydown');
+        isMouseDown = true;
+        //calculated distance from
+        closestReferencePoint = that.getClosestReferencePoint(e.pageX, e.pageY, that.referencePointsX, that.referencePointsY).closestRefIndex;
+        //console.log('Closest Ref Index: ' + closestReferencePoint);
+    };
+
+    this.mouseup = function(e) {
+        //console.log('keyup');
+        $('body').css('cursor','default');
+        closestReferencePoint = -1;
+        isMouseDown = false;
+    };
+
+    this.mousemove = function(e) {
+
+        if(isMouseDown){
+
+            $('body').css('cursor','none');
+            //  console.log(e.pageX + ", " + e.pageY);
+            that.referencePointsX[closestReferencePoint] = (e.pageX - this.graphParams.leftOffset);
+            that.referencePointsY[closestReferencePoint] = this.graphParams.graphHeight - e.pageY;
+
+            //console.log('Reference X: ' + (referencePointsX[closestReferencePoint]));
+        }
+
+    };
+    this.mousemoveNoClick = function(e) {
+        closestReferenceInfo = that.getClosestReferencePoint(e.pageX, e.pageY, referencePointsX, referencePointsY);
+    };
+
+    this.getReferencePoints = function() {
+        return [ [referencePointsX[0], referencePointsY[0]], [referencePointsX[1], referencePointsY[1]] ];
+    };
+
+    this.setPoints = function(pointsX, pointsY) {
+        this.pointsX = pointsX;
+        this.pointsY = pointsY;
+    };
+
+    this.init();
+}
